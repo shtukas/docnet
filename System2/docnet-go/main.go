9cdff64e715fb60852493535e3dfaccf7e98929c
Pascal Honoré <pascal@alseyn.net>
2021-04-28 21:11:39 +0100

    rivo/tview as ui library

--- System2/docnet-go/main.go
+++ System2/docnet-go/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"github.com/rivo/tview"
+)
 
 func main() {
-    fmt.Println("Hello, world.")
+	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
 }
