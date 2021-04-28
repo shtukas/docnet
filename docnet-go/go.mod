012176f8aa326db643fcc72b4b76b871a6134423
Pascal Honoré <pascal@alseyn.net>
2021-04-28 23:28:23 +0100

    Interface updates

--- docnet-go/go.mod
+++ docnet-go/go.mod
@@ -2,4 +2,8 @@ module github.com/shtukas/docnet
 
 go 1.16
 
-require github.com/rivo/tview v0.0.0-20210427112837-09cec83b1732 // indirect
+require (
+	github.com/gdamore/tcell v1.4.0 // indirect
+	github.com/gdamore/tcell/v2 v2.2.1 // indirect
+	github.com/rivo/tview v0.0.0-20210427112837-09cec83b1732 // indirect
+)
--- docnet-go/main.go
+++ docnet-go/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+
+	app := tview.NewApplication()
+
+	main := tview.NewTextView().
+			SetTextAlign(tview.AlignLeft).
+			SetText("Hello World")
+
+	commandField := tview.NewInputField().
+		SetLabel("> ").
+		SetFieldWidth(0).
+		SetFieldBackgroundColor(tcell.NewHexColor(0)).
+		SetChangedFunc(func(text string) {
+			
+		}).
+		SetDoneFunc(func(key tcell.Key) {
+
+		})
+
+	grid := tview.NewGrid().
+		SetRows(-1, 1).
+		SetColumns(-1).
+		SetBorders(true).
+		AddItem(main, 0, 0, 1, 1, 0, 0, false).
+		AddItem(commandField, 1, 0, 1, 1, 0, 0, true)
+
+	if err := app.SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
 		panic(err)
 	}
 }
