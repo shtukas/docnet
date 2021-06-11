c360dbdef5d855790c6e20b58d35f77995b7b680
Pascal Honoré <pascal@alseyn.net>
2021-06-11 18:13:32 +0100

    Playing

--- docnet-go/main.go
+++ docnet-go/main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"github.com/gdamore/tcell/v2"
-	"github.com/rivo/tview"
+	"github.com/rivo/tview" // https://pkg.go.dev/github.com/rivo/tview
 )
 
 func main() {
 
 	app := tview.NewApplication()
 
-	main := tview.NewTextView().
-			SetTextAlign(tview.AlignLeft).
-			SetText("Hello World")
+	mainView := tview.NewTextView().
+		SetTextAlign(tview.AlignLeft).
+		SetText("Hello World")
 
 	commandField := tview.NewInputField().
 		SetLabel("> ").
 		SetFieldWidth(0).
 		SetFieldBackgroundColor(tcell.NewHexColor(0)).
 		SetChangedFunc(func(text string) {
-			
+			if text == "exit" {
+				app.Stop()
+				return
+			}
+			mainView.SetText(text)
 		}).
 		SetDoneFunc(func(key tcell.Key) {
 
@@ -28,7 +32,7 @@ func main() {
 		SetRows(-1, 1).
 		SetColumns(-1).
 		SetBorders(true).
-		AddItem(main, 0, 0, 1, 1, 0, 0, false).
+		AddItem(mainView, 0, 0, 1, 1, 0, 0, false).
 		AddItem(commandField, 1, 0, 1, 1, 0, 0, true)
 
 	if err := app.SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
