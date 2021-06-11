8e53f892d55781482eadcf363eb283ef9f2c0f63
Pascal Honoré <pascal@alseyn.net>
2021-06-11 19:04:38 +0100

    Learning to modify the elements of the Grid

--- docnet-go/main.go
+++ docnet-go/main.go
@@ -5,15 +5,38 @@ import (
 	"github.com/rivo/tview" // https://pkg.go.dev/github.com/rivo/tview
 )
 
+func updateGridContents(grid *tview.Grid, mainElement tview.Primitive, commandField tview.Primitive) {
+	grid.
+		Clear().
+		SetRows(-1, 1).
+		SetColumns(-1).
+		SetBorders(true).
+		AddItem(mainElement, 0, 0, 1, 1, 0, 0, false).
+		AddItem(commandField, 1, 0, 1, 1, 0, 0, true)
+}
+
 func main() {
 
 	app := tview.NewApplication()
 
-	mainView := tview.NewTextView().
+	grid := tview.NewGrid()
+
+	textView := tview.NewTextView().
 		SetTextAlign(tview.AlignLeft).
 		SetText("Hello World")
 
-	commandField := tview.NewInputField().
+	list := tview.NewList().
+		AddItem("List item 1", "Some explanatory text", 'a', nil).
+		AddItem("List item 2", "Some explanatory text", 'b', nil).
+		AddItem("List item 3", "Some explanatory text", 'c', nil).
+		AddItem("List item 4", "Some explanatory text", 'd', nil).
+		AddItem("Quit", "Press to exit", 'q', func() {
+			app.Stop()
+		})
+
+	commandField := tview.NewInputField()
+
+	commandField.
 		SetLabel("> ").
 		SetFieldWidth(0).
 		SetFieldBackgroundColor(tcell.NewHexColor(0)).
@@ -22,18 +45,18 @@ func main() {
 				app.Stop()
 				return
 			}
-			mainView.SetText(text)
+			if text == "list" {
+				updateGridContents(grid, list, commandField)
+				return
+			}
+			updateGridContents(grid, textView, commandField)
+			textView.SetText(text)
 		}).
 		SetDoneFunc(func(key tcell.Key) {
 
 		})
 
-	grid := tview.NewGrid().
-		SetRows(-1, 1).
-		SetColumns(-1).
-		SetBorders(true).
-		AddItem(mainView, 0, 0, 1, 1, 0, 0, false).
-		AddItem(commandField, 1, 0, 1, 1, 0, 0, true)
+	updateGridContents(grid, textView, commandField)
 
 	if err := app.SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
 		panic(err)
