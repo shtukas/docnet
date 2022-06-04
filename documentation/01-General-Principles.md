5ec1ffba217d7b4ca3e9913c58ff3750b69df227
Pascal Honoré <pascal@alseyn.net>
2022-06-04 18:39:15 +0100

    create the .docnet directory

--- documentation/01-General-Principles.md
+++ documentation/01-General-Principles.md
@@ -7,4 +7,6 @@ The entirely of the DocNet dataset is a collection of files, with extension `.fx
 Each fx13 file is a sqlite3 database with just one table called `_data_`. This simplicity is for both backward and forward compatibility, and all the data and metadata and attributes of those files will be in this one database table. 
 
 
+### User Data
 
+All user data is located in `~/docnet`
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::fs;
 use std::io;
+use std::path::PathBuf;
+use std::process::exit;
 use std::str::FromStr;
+use std::path::Path;
+use std::fs::{DirBuilder};
+use std::env;
 
 //use serde::{Deserialize, Serialize};
 use serde_json::{Value as JSONValue};
@@ -41,20 +46,39 @@ fn get_librarian_objects(conn: &Connection) -> Result<Vec<LibrarianObject>, Erro
     Ok(objects)
 }
 
-fn main() -> Result<(), Error> {
-    println!("Welcome to DotNet");
-
-    //println!("Please enter your name: ");
-    //let mut buffer = String::new();
-    //io::stdin().read_line(&mut buffer)?;
-
+fn example_of_sqlite_database_use() -> Result<(), Error> {
     let conn = Connection::open("/Users/pascal/Galaxy/DataBank/Stargate/objects-store.sqlite3")?;
-
     let objects = get_librarian_objects(&conn).unwrap();
-
     for object in objects {
         println!("uuid from database: {}, {:?}", object.uuid, object.raw_object)
     }
+    Ok(())
+}
 
+// This function checks that the user's docnet folder exists and if it doesn't
+// proposes to create it. Returns a boolean indicating if the directory exists or not.
+fn ensure_docnet_folder() -> bool {
+    let path = "/Users/pascal/.docnet"; // TODO: search for the user's home directory
+    let exists = Path::new(path).exists();
+    if !exists && ask_question_answer_as_boolean("The .docnet data folder doesn't exists, going to create it").unwrap() {
+        println!("You have chosen to create the directory");
+        fs::DirBuilder::new().create(path).unwrap();
+    }
+    Path::new(path).exists()
+}
+
+fn ask_question_answer_as_boolean(question: &str) -> Result<bool, std::io::Error> {
+    println!("{} (yes, no) : ", question);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    Ok(input.trim() == "yes")
+}
+
+fn main() -> io::Result<()> {
+    println!("Welcome to DotNet");
+    if !ensure_docnet_folder() {
+        println!("Could not find or create the docnet folder. Aborting.");
+        exit(0);
+    }
     Ok(())
 }
